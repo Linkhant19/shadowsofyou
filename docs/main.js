96f99fd6daa4f8ca06376fd8bdf6e72beac98afc
Lin Khant Ko <lkk19@bu.edu>
2024-03-17 22:40:55 -0400

    added links for images. updated the images

---
+++ docs/main.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    document.getElementById("a").addEventListener("click", gotoa);
+    document.getElementById("b").addEventListener("click", gotob);
+    document.getElementById("c").addEventListener("click", gotoc);
+    document.getElementById("d").addEventListener("click", gotod);
+}
+
+function gotoa() {
+    window.open("https://linkhant19.github.io/shadowsofyou_theboywholived/", "_blank");
+}
+
+function gotob() {
+    window.open("https://linkhant19.github.io/shadowsofyou_nevermore/", "_blank");
+}
+
+function gotoc() {
+    window.open("https://linkhant19.github.io/shadowsofyou_heavenontheothersideoftheworld/", "_blank");
+}
+
+function gotod() {
+    window.open("https://linkhant19.github.io/shadowsofyou_shadowsofus/", "_blank");
+}
